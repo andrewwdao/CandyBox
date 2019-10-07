57580ccd06d3d94c77a743598b529c4129f31645
Minh An Dao <minhan7497@gmail.com>
2019-10-07 12:33:30 +0700

    change directory for pi

--- RaspberryPi_main/Python/motor.py
+++ RaspberryPi_main/Python/motor.py
@@ -56,7 +56,7 @@ class StepperControl:
         control_dir = "./main" + str(self.turns) + str(self.speed)
         try:
             #retcode = subprocess.call(control_dir, shell=True)
-            retcode = subprocess.Popen(["./main 3 100"])
+            retcode = subprocess.Popen(["./main 3 100"], shell=True)
             if retcode < 0:
                 print("Child was terminated by signal", -retcode, file=sys.stderr)
             else:
