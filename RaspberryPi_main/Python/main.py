92c3838bfe5fc18e79156b6601c8d21a41a25338
Minh An Dao <minhan7497@gmail.com>
2019-10-07 10:33:36 +0700

    change directory for pi

--- RaspberryPi_main/Python/main.py
+++ RaspberryPi_main/Python/main.py
@@ -37,7 +37,10 @@ if __name__ == "__main__":
     # ----------------------------Loop
     while True:
         PiAudioRecord.start()
-        if webEmpath.check(PiAudioRecord.des_wav, JOY_THRESHOLD):
-            PiAudioRecord.save_joy(webEmpath.joy_now())
-            #stepper.move()
-            print("Candy Drop!")
+        try:
+            if webEmpath.check(PiAudioRecord.des_wav, JOY_THRESHOLD):
+                PiAudioRecord.save_joy(webEmpath.joy_now())
+                stepper.move()
+                print("Candy Drop!")
+        except:
+            print("some fucking error!")
