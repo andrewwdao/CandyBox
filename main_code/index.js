a550ec152f9d1bed40b1d0c17653f1411ab44ebb
Minh An Dao <minhan7497@gmail.com>
2019-10-21 16:26:31 +0700

    add subprocess for node.js

--- main_code/index.js
+++ main_code/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views','./nodejs/views');
+app.set('views','./views');
 app.use(express.static('public'));
 
 var server = require("http").Server(app);
